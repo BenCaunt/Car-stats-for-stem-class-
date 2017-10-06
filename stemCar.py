8ec5050fff4d483bcefaf6c435abc0fb1b282ac7
catman1232 <bdcaunt@gmail.com>
2017-10-05 21:05:26 -0400

    stem car project

---
+++ stemCar.py
@@ -0,0 +1,20 @@
+#change to your car
+time = 3.586  # seconds
+distance = 5.81  #meters
+mass = 0.0198 #kg 
+
+#equation
+velocity = distance/time
+mph = (velocity*2.23)
+acceleration = velocity/time
+p = mass*velocity
+
+#output
+print("velocity")
+print(velocity)
+print("speed in mph")
+print(mph)
+print("acceleration")
+print(acceleration)
+print("momentum")
+print(p)
